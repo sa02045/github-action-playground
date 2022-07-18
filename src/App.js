15d7e243c25f5d23c0c87a9585cb4b5145f98ef4
sa02045 <sa02045@naver.com>
2022-07-18 16:58:09 +0900

    qa

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        v0.1.1
+        v0.1.3333
         <a
           className="App-link"
           href="https://reactjs.org"
