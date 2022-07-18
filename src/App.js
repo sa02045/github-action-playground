197b7a6551050c8d2ec37adee5029a52aed0b298
sa02045 <sa02045@naver.com>
2022-07-18 17:04:38 +0900

    patch version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        v0.4.1
+        patch
         <a
           className="App-link"
           href="https://reactjs.org"
