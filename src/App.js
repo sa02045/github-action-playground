b086d97e336b0ccea2a3aa4e62000b6a8aec7323
sa02045 <sa02045@naver.com>
2022-07-18 16:27:32 +0900

    release test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        test
         <a
           className="App-link"
           href="https://reactjs.org"
