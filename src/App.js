d69ecf1749b9e35e62fb712609031aa50cf713a9
sa02045 <sa02045@naver.com>
2022-07-18 16:49:08 +0900

    v0.1.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        v0.1.1
+        testv0.1.1
         <a
           className="App-link"
           href="https://reactjs.org"
