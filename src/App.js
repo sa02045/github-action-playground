cf10d1a9fbd20ccbd208fdbfa0123ab0e6a51fd4
Devjo <50866506+sa02045@users.noreply.github.com>
2022-07-20 17:30:24 +0900

    feat103 (#37)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
     </div>
   );
